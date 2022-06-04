4c0a84863ddc54652e407d26fb33870c79b88613
shargu <88038585+shargu@users.noreply.github.com>
2022-06-04 19:18:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dream</title>
+       
+        <link rel="stylesheet" href="style.css">
+        <!-------Poppins-google-fonts------->
+        <link rel="preconnect" href="htpps://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com?css2?
+        family=Poppinns:wght@100;200;300;400;500;600;700;800&display=swap" rel="styleshet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=IBM+Plex+Sans&family=Poppins:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="all.min.css">
+    </head>
+    <body>
+       
+    <div class="header">
+        <div class="hero">
+          <nav>
+            <img src="MarkNet-removebg-preview.png" class="logo">
+            <ul>
+                <li><a href="index.php" class="btn">Acasa</a></li>
+                <li><a href="portofoliu.php" class="btn">Portofoliu</a></li>
+                <li><a href="clientii.php" class="btn">Clientii</a></li>
+                <li><a href="servicii.php" class="btn">Servicii</a></li>
+            </ul> 
+            <a href="login.php" class="btn1">Inregistreaza-te</a>
+          </nav>
+</div>
+</div>
+          
+          <div class="row">
+            <div class="col-1">
+              <img class="team" src="Team goals-bro.png"  >
+              
+              
+          </div>
+          <div class="col-2">
+            <h1 class="central">
+              
+              MarkNet este soluția ta NR.1 în rezolvarea problemelor  de marketing.
+          </h1>
+           
+        </div>
+        <div class="col-3">
+          <p>MarkNet este o agenție de marketing online și, în acest context, îți lansăm o provocare: cum ar fi ca același buget de promovare să îți aducă rezultate duble sau chiar triple, care se vor simți în cifra de afaceri a companiei tale? Profitul ar atinge cote noi, iar tu ai putea să te relaxezi în sfârșit, știind că faci ceea ce trebuie.</p>
+        </div>
+        
+        </div>
+      <!---------About us --------->
+      <section id="about-us">
+        <div class="container">
+          <h1 class="title" >De ce să ne alegi ?</h1>
+          <div class="row">
+            <div class="col-4 about-us">
+             <p class="question">De ce suntem diferiți ?</p>
+             <ul class="lista">
+               <li>Înțelegem mai întâi obiectivul de afaceri al clientului nostru</li>
+               <li>Credem în realizarea afacerilor cu onestitate</li>
+               <li>Livrăm la timp</li>
+               <li>Realizăm o strategie de conținut câștigătoare</li>
+               <li>Vă protejăm reputația online</li>
+               <li>Creștere rapidă și rezultate impecabile</li>
+               <li>Dezvoltare multilaterală pentru business-ul tău</li>
+                <li>Transparență și optimizări în timp real</li>
+               
+             </ul>
+             <img class="together"src="Partnership-bro.png">
+             <img src="Setup-amico.png" class="setup">
+             <h1 class="level">Treci la nivelul urmator</h1>
+            <img src="wordpress.png" class="wordpress3"> <h2 class="wordpress">WordPress</h2>
+             <p class="wordpress1">Folosim sisteme de content management precum <br>WordPress sau Joomla pentru a va oferi un site web<br> pe carea il puteti administra chiar dumneavoastra.</p>
+            <h1 class="seo">SEO din prima</h1>
+           <img src="seo.png" class="seo2"> <p class="seo1">Inca din faza de proiectare facem actiunile SEO<br>pentru design, continut, performanta si adaptabilitate <br>site la dispozitive, mobil pentru ca site-ul tau sa fie <br> prietenos cu motoarele de cautare</wp>
+            <h1 class="rocket">Viteza mare de incarcare</h1>
+            <img src="rocket.png" class="rocket2"><p class="rocket1">Livram site-uri cu scor mare in Google Page Insights <br> pentru a-ti oferi o rata de conversie mai mare si o <br>experienta placuta pentru utilizatori.<p>
+          </div>
+          </div>
+        </div>
+      </section>
+      <!--------about us end--------->
+    <!-----------services --------->
+    <section id="services">
+      <div class="container">
+        <h1 class="servicestitle" >Dacă ești în căutarea unei soluții care să dea rezultate peste noapte, te afli în locul nepotrivit.</h1>
+        <div class="row">
+          <div class="col-5 services">
+           <p class="servicesp"><strong>Succesul la plic nu este promisiunea noastră și nici nu cunoaștem vreo formulă magică care să crească vânzările instant</strong> .
+           </p ><p class="servicesp">Toate tehnicile pe care le utilizăm sunt tehnici bine fundamentate în teoriile și practicile din domeniu. Aplicarea acestora presupune un proces care, deși uneori dificil, este întotdeauna profitabil.</b>
+
+            Bineînțeles, dacă îți dorești rezultate cu adevărat surprinzătoare, va trebui să facem câteva eforturi comune. Pentru ca fiecare campanie să funcționeze, este necesară și implicarea ta. Împreună, putem identifica punctele tari ale companiei, profilul consumatorilor și direcțiile de promovare. </p>
+           <img class="work" src="Developer activity-bro.png">
+          </div>
+        </div>
+      </div>
+    
+    <div class="services1">
+      <div class="services2">
+       <h2>Cu ce altceva te putem ajuta </h2>
+      </div>
+
+      <div class="box1">
+        <div class="card1 cd01">
+          <img src="host.png">
+         <h5>Gazduire Web</h5>
+         <div class="pha">
+           <p1>Livram o solutie completa de gazduire web SSD, inregistrare domeniu, certificat SSL cu garantie de uptime 99.9%.</p1>
+         </div>
+        </div>
+      </div>
+
+      <div class="card1 cd02">
+        <img src="arrow.png">
+         <h5>Optimizare SEO</h5>
+         <div class="pha">
+           <p1>Te ajutam sa ocupi o pozitie fruntasa in cautarile pe Google printr-o strategie de optimizare bine pusa la punct si cu rezultate pe termen lung.</p1>
+         </div>
+        </div>
+      </div>
+
+      <div class="card1 cd03">
+      <img src="megaphone.png">
+         <h5>Social Media Marketing</h5>
+         <div class="pha">
+           <p1>Cream si optimizam campanii de promovare pe Facebook si Instagram folosind postari cu un impact mare si cu sanse mari de conversii targetand corect publicul tinta.</p1>
+         </div>
+        </div>
+      </div>
+
+      <div class="card1 cd04">
+      <img src="report.png">
+         <h5>Analiza si Rapoarte</h5>
+         <div class="pha">
+           <p1>Cream o analiza a site-ului tau si intocmim un raport impreuna cu o solutie pentru problemele descoperite.</p1>
+         </div>
+        </div>
+      </div>
+
+      <div class="card1 cd05">
+      <img src="grow.png">
+         <h5>Google Ads si campanii PPC</h5>
+         <div class="pha">
+           <p1>Prin serviciile noastre de Google Ads (AdWords) si PPC (Pay Per Click), ne asiguram ca produsele si serviciile tale sunt doar la o clipa si un click distanta de a fi achizitionate.</p1>
+         </div>
+        </div>
+      </div>
+
+      <div class="card1 cd06">
+      <img src="brand.png">
+         <h5>Branding</h5>
+         <div class="pha">
+           <p1>Pentru afacerile la inceput de drum si nu numai oferim servicii de branding ce includ logo-uri, flyere, materiale promotionale, etc.</p1>
+         </div>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+<!------------------services end-------->
+<!-------portofoliu---------->
+<div id="portofolio">
+  <h2 class="proiectetitle">Ultimele noastre proiecte</h2>
+  <p class="proiecte">Mai jos sunt prezentate doar cateva din modele de website-uri create de firma de web design TMC Group.
+    Pentru mai multe detalii legate de portofoliu nu ezitati sa ne contactati.</p>
+   
+    <div class="container">
+      <div class="card">
+        <div class="imgBx">
+          <img src="photoportofoliu.png">
+        </div>
+        <div class="content">
+          <a href="food website/index.php">
+          <button class="learn-more">
+            <span class="circle" aria-hidden="true">
+            <span class="icon arrow"></span>
+            </span>
+            <span class="button-text">Vezi site-ul</span>
+          </button></a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="imgBx">
+          <img src="coffe.png">
+        </div>
+        <div class="content">
+          <a href="coffe/index.php">
+          <button class="learn-more">
+            <span class="circle" aria-hidden="true">
+            <span class="icon arrow"></span>
+            </span>
+            <span class="button-text">Vezi site-ul</span>
+          </button></a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="imgBx">
+          <img src="construction.png">
+        </div>
+        <div class="content">
+         <a href="construction/index.php"> <button class="learn-more">
+            <span class="circle" aria-hidden="true">
+            <span class="icon arrow"></span>
+            </span>
+            <span class="button-text">Vezi site-ul</span>
+          </button></a>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</section>
+<!-----portofoliu end----->
+<!----- footer------>
+<img src="wave1.png" class="wave">
+<img src="MarkNet-removebg-preview.png" class="footerimg">
+<section>
+<div class="footer">
+  
+  <a href="https://www.instagram.com/mar_knet/" class="instagram"><img src="instagram.png" class="instagram"></a>
+  <a href="https://www.facebook.com/Marknet-FE-107938538565276" class="facebook"><img src="facebook2.png"></a>
+  <ul>
+  <i class="fa-solid fa-greater-than" ></i><li><a href="index.php" class="test02" >Acasa</a></li>
+  <i class="fa-solid fa-greater-than"></i><li><a href="portofoliu.php"class="test02" >Portofoliu</a></li>
+  <i class="fa-solid fa-greater-than"></i><li><a href="clientii.php"class="test02" >Clientii</a></li>
+  <i class="fa-solid fa-greater-than"></i><li><a href="servicii.php"class="test02" >Servicii</a></li>
+  <i class="fa-solid fa-greater-than"></i><li><a href="login.php"class="test02" >Inregistreaza-te</a></li>
+
+  <i class="fa-solid fa-greater-than test05" ></i><li><a href="promovare.php" class="test03" >Promovare Online</a></li>
+  <i class="fa-solid fa-greater-than test05" ></i><li><a href="portofoliu.php"class="test03" >Creare magazin online</a></li>
+  <i class="fa-solid fa-greater-than test05 " ></i><li><a href="clientii.php"class="test03" >Creare site de prezentare</a></li>
+  <i class="fa-solid fa-greater-than test05" ></i><li><a href="servicii.php"class="test03" >Creare brand si logo</a></li>
+  <i class="fa-solid fa-greater-than test05" ></i><li><a href="login.php"class="test03" >Optimizare SEO</a></li>
+</ul>
+
+</div>
+<p class="employ">Websites promote you 24/7: No employee <br>will do that</p>
+<p class="contact">Contact</p>
+<p class="contact2"><b>Email</b>:tmcgroup431@gmail.com</p>
+</section>
+<iframe class="map"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2813.7931955553363!2d26.820582615547018!3d45.150785479098595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b15fdc06fff047%3A0xde085de3ab33b67c!2sColegiul%20Economic%20Buz%C4%83u!5e0!3m2!1sro!2sro!4v1650732897781!5m2!1sro!2sro" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<!-----footer end------>
+</body>
+ </html>
